d901a1a78d8744f0e8c6833704b65e9e10494148
Benjamin Mentink <300079207@fvtc.edu>
2023-11-28 14:19:59 -0600

    Added ShoppingCart BL Model ShoppingCart Manager ShoppingCart controller ShoppingCart Controller Index

---
+++ BJM.ProgDec.BL.Models/ShoppingCart.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJM.ProgDec.BL.Models
+{
+    public class ShoppingCart
+    {
+        const double ITEM_COST = 120.03;
+        const double TAX_RATE = .055;
+
+        public List<Declaration> Items {  get; set; } = new List<Declaration>();
+        public int NumberOfItems 
+        {
+            get { return Items.Count; }
+        }
+        [DisplayFormat(DataFormatString = "(0:C)")] // formats to currency
+        public double SubTotal { get { return Items.Count * ITEM_COST; } }
+        [DisplayFormat(DataFormatString = "(0:C)")] // formats to currency
+        public double Tax { get { return SubTotal * TAX_RATE; } }
+        [DisplayFormat(DataFormatString = "(0:C)")] // formats to currency
+        public double Total { get { return SubTotal * Tax; } }
+    }
+}
---
+++ BJM.ProgDec.BL/ShoppingCartManager.cs
@@ -0,0 +1,44 @@
+﻿using BJM.ProgDec.BL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BJM.ProgDec.BL
+{
+   public static class ShoppingCartManager
+    {
+        public static void Add(ShoppingCart cart, Declaration declaration)
+        {
+            if (cart != null)
+            {
+                cart.Items.Add(declaration);
+            }
+        }
+        public static void Remove(ShoppingCart cart, Declaration declaration)
+        {
+            if (cart != null)
+            {
+                cart.Items.Remove(declaration);
+            }
+        }
+        public static void Checkout(ShoppingCart cart)
+        {
+              // make a new order
+              // set order fields as needed
+              // foreach(movie item in  cart.Items)
+
+              // Make a new order Item
+              // Set the orderItem firelds from the  item
+              // order.OrderIterms.Add(OrderItems)
+
+              // order<amager.Insert(order)
+              // update the in stk quantity from tblMovie
+              // if(tblMovie.count > 0)
+            cart = new ShoppingCart();
+
+        }
+    }
+    
+}
